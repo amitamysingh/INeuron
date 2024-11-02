f620371114cd03a9c2b26c9961bf3ea151877293
Amit <amrit546great@gmail.com>
2024-11-02 19:17:50 +0530

    Commiting Junit newer changes

--- Java_09_Junit/Junit_Project_02/src/test/java/ineuron/test/TestBankLoanService.java
+++ Java_09_Junit/Junit_Project_02/src/test/java/ineuron/test/TestBankLoanService.java
@@ -1,25 +1,27 @@
 package ineuron.test;
 
 import ineuron.services.BankLoanService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("TestBankLoanServiceClass")
+@TestMethodOrder(value=MethodOrderer.OrderAnnotation.class)
 public class TestBankLoanService {
 
-    private BankLoanService bankLoanService;
+    private static BankLoanService bankLoanService;
 
-    @BeforeEach
-    public void setUp(){
+    @BeforeAll
+    public static void setUp(){
         System.out.println("TestBankLoanService.setUp()");
         bankLoanService = new BankLoanService();
     }
 
+    @DisplayName("TestingWithSmallNumbers")
     @Test
+    @Order(1)
     public void testcalculateSimpleIntrestWithSmallNumbers(){
         System.out.println("TestBankLoanService.testcalculateSimpleIntrestWithSmallNumbers()");
         float actualOutput = bankLoanService.calculateSimpleIntrest(100000, 2, 12);
@@ -35,7 +37,10 @@ public class TestBankLoanService {
 
     }
 
+    @DisplayName("Testing with Big Numbers")
+    @Disabled
     @Test
+    @Order(2)
     public void testcalculateSimpleIntrestWithBigNumbers(){
         System.out.println("TestBankLoanService.testcalculateSimpleIntrestWithBigNumbers()");
 
@@ -47,7 +52,9 @@ public class TestBankLoanService {
         assertEquals(expectedOutput,actualOutput, 0.5f, "Results are not matching");
     }
 
+    @DisplayName("Testing with Invalid inputs")
     @Test
+    @Order(3)
     public void testcalculateSimpleIntrestWithInvalidInput(){
         System.out.println("TestBankLoanService.testcalculateSimpleIntrestWithInvalidInput()");
 
@@ -55,7 +62,10 @@ public class TestBankLoanService {
 
     }
 
+    @DisplayName("Testing with Time Interval")
+    @Disabled
     @Test
+    @Order(4)
     public void testcalculateSimpleIntrestWithinTimeInterval(){
         System.out.println("TestBankLoanService.testcalculateSimpleIntrestWithTimeInterval()");
 
@@ -63,8 +73,8 @@ public class TestBankLoanService {
 
     }
 
-    @AfterEach
-    public void cleanUp(){
+    @AfterAll
+    public static void cleanUp(){
         System.out.println("TestBankLoanService.cleanUp()");
         bankLoanService = null;
     }
